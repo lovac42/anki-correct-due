c63f07014dedd9fd9558e7fe7f26afd2ff18c024
lovac42 <42665127+lovac42@users.noreply.github.com>
2019-04-23 18:19:37 -0400

    Update redue.py
    2.0 port, options, comments

--- redue.py
+++ redue.py
@@ -1,13 +1,27 @@
 # FROM: https://github.com/Arthur-Milchior/anki-correct-due
-# Modified version by lovac42 for Anki 2.0
+# Modified version by lovac42 for Anki 2.0 (untest on 2.1)
+
+# Anki's due time is 32bit, but the due field on new cards also
+# allows negative numbers. To prevent overflow, during db checkup,
+# it is capped at a safe limit of 1,000,000 or about 22bits.
+
+# PSEUDOCODE:
+# foreach card in whole_collection {
+#    card.due = max(1000000, card.due)
+# }
+
+# All cards affected by this will loose their sorting during review.
+# This addon repositions the due numbers before the checkup.
 
 
 # =====================================
 
-AUTOMATIC_SCAN_ON_DB_CHECK = False
+AUTOMATIC_SCAN_BEFORE_DB_CHECKUP = False
+
+# Priority: due, sibling order, creation time
+REORDER_BY="due,ord,id"
 
 # =====================================
-
 
 import anki
 from anki.hooks import wrap
@@ -25,7 +39,7 @@ def redue(col):
         return
 
     #doesn't show up on small db
-    tooltip(_("found cards with dues more than 666000, repositioning now."), period=3000)
+    tooltip(_("found cards with dues more than 666000, repositioning now."), period=2000)
 
 
     redline = col.db.scalar(
@@ -34,27 +48,27 @@ def redue(col):
 
     if redline < 65536: #process overflows only
         query="""select id from cards where type=0 
-                and due>666000 order by due,ord"""
+                and due>666000 order by %s"""%REORDER_BY
 
     else: #process all
-        query = "select id from cards where type=0 order by due,ord"
+        query = "select id from cards where type=0 order by %s"%REORDER_BY
         redline = 0
 
     cids = col.db.list(query)
     col.sched.sortCards(cids, start=redline, shift=True)
 
-
+    # Reset pos counter
     col.conf['nextPos'] = col.db.scalar(
         "select max(due)+1 from cards where type = 0") or 0
 
+    # If deck conf was set to randomize review order
     dconfs = col.decks.dconf
-
     random_dconfs = [dconf for dconf in dconfs.values() if dconf["new"]['order'] == NEW_CARDS_RANDOM]
     for dconf in random_dconfs:
         col.sched.resortConf(dconf)
 
 
-if AUTOMATIC_SCAN_ON_DB_CHECK:
+if AUTOMATIC_SCAN_BEFORE_DB_CHECKUP:
     anki.collection._Collection.fixIntegrity = wrap(anki.collection._Collection.fixIntegrity,redue,"before")
 else:
     action = QAction(mw)
@@ -62,3 +76,4 @@ else:
     mw.form.menuTools.addAction(action)
     action.triggered.connect(lambda:redue(mw.col))
 
+    
